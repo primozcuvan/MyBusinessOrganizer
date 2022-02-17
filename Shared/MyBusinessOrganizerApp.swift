3d715a02c28d67b640a6cd0e0045fd18d4e71a99
Primoz Cuvan <primoz.cuvan@gmail.com>
2022-02-17 09:39:48 +0100

    Initial Commit

---
+++ Shared/MyBusinessOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBusinessOrganizerApp.swift
+//  Shared
+//
+//  Created by Primoz Cuvan on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyBusinessOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
