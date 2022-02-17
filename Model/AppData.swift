d97f635c339682abe86e421ca8d2706f5bf16109
Primoz Cuvan <primoz.cuvan@gmail.com>
2022-02-17 13:54:04 +0100

    Add AppData

---
+++ Model/AppData.swift
@@ -0,0 +1,18 @@
+//
+//  AppData.swift
+//  MyBusinessOrganizer (iOS)
+//
+//  Created by Primoz Cuvan on 17/02/2022.
+//
+
+import Foundation
+
+struct AppData {
+    
+    static var contacts: [Contact] = [
+        Contact(name: "John", phoneNumber: "+1 650 7327717", location: "John's home"),
+        Contact(name: "Meggie", phoneNumber: "+1 650 7327607", location: "Meggie's home"),
+        Contact(name: "Martin", phoneNumber: "+1 650 7322207", location: "Martin's home")
+    ]
+    
+}
